e1df0e51038a35a78b771055321763256cbc1a53
Guilherme Dias Gomes <guilhermediasgomes2@gmail.com>
2023-09-15 15:06:29 -0300

    Adicionando imagens aos produtos

--- src/components/listaProdutos.jsx
+++ src/components/listaProdutos.jsx
@@ -2,26 +2,31 @@ export const listaProdutos = [
     {
         id: 1,
         nome: "A22",
+        img:"https://picsum.photos/100/100",
         preco: 880
     },
     {
         id: 2,
         nome: "Iphone 13",
+        img:"https://picsum.photos/100/100",
         preco: 3000
     },
     {
         id: 3,
         nome: "Ipad Mini",
+        img:"https://picsum.photos/100/100",
         preco: 2200
     },
     {
         id: 4,
         nome: "Sansung S20",
+        img:"https://picsum.photos/100/100",
         preco: 3000
     },
     {
         id: 5,
         nome: "Ipad Pro",
+        img:"https://picsum.photos/100/100",
         preco: 2500
     },
 ]
