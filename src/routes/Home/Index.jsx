d68fa34085fec527e89b8e3a10a43ac52ba5e543
Guilherme Dias Gomes <guilhermediasgomes2@gmail.com>
2023-09-15 15:08:40 -0300

    adicionando css module e formatações da aba de produtos

--- src/routes/Home/Index.jsx
+++ src/routes/Home/Index.jsx
@@ -1,10 +1,57 @@
+import { listaProdutos } from "../../components/listaProdutos";
+import { Link } from "react-router-dom";
+import styles from "../Produtos.module.css";
+
 export default function Home() {
 
-    return(
+    return (
         <main>
             <h1>| Home |</h1>
-            
+            <table className={styles.tblEstilo}>
+
+                <thead className={styles.tblHeader}>
+                    <tr>
+                        <th>ID</th>
+                        <th>NOME</th>
+                        <th>IMAGEM</th>
+                        <th>PREÇO</th>
+                        <th>EDITAR/EXCLUIR</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {listaProdutos.slice(0, 2).map((item, indice) => (
+                        <tr key={indice} className={styles.tblRow}>
+                            <td>{item.id}</td>
+                            <td>{item.nome}</td>
+                            <td>
+                                <img src={`${item.img}`} alt={`${item.nome}`} />
+                            </td>
+                            <td>{item.preco}</td>
+                            <td>
+                                {" "}
+                                <Link to={`/editar/produtos/${item.id}`}>
+                                    {" "}
+                                    Editar
+                                </Link>{" "}
+                                /{" "}
+                                <Link to={`/excluir/produtos/${item.id}`}>
+                                    {" "}
+                                    Excluir
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+
+                <tfoot>
+                    <tr>
+                        <td colSpan={6}>Produtos em Promoção</td>
+                    </tr>
+                </tfoot>
+
+            </table>
         </main>
     )
-    
+
 }
--- src/routes/Produtos/Index.jsx
+++ src/routes/Produtos/Index.jsx
@@ -1,19 +1,59 @@
-import { Link } from "react-router-dom"
-import { listaProdutos } from '../../components/listaProdutos'
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../components/listaProdutos";
+import styles from "../Produtos.module.css";
 
 export default function Produtos() {
+  document.title = "Produtos";
 
-    return(
-        <main>
-            <h1>Produtos</h1>
-            {listaProdutos.map(prod => (
-                <div key={prod.id}>
-                    <Link to={`/editar/produtos/${prod.id}`}>
-                        Editar o produto: {prod.nome}
-                    </Link>
-                </div>
+  return (
+    <>
+      <h1>Lista de Produtos</h1>
+      <div>
+        <table className={styles.tblEstilo}>
+
+          <thead className={styles.tblHeader}>
+            <tr>
+              <th>ID</th>
+              <th>NOME</th>
+              <th>IMAGEM</th>
+              <th>PREÇO</th>
+              <th>EDITAR/EXCLUIR</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {listaProdutos.map((item, indice) => (
+              <tr key={indice} className={styles.tblRow}>
+                <td>{item.id}</td>
+                <td>{item.nome}</td>
+                <td>
+                  <img src={`${item.img}`} alt={`${item.nome}`} />
+                </td>
+                <td>{item.preco}</td>
+                <td>
+                  {" "}
+                  <Link to={`/editar/produtos/${item.id}`}>
+                    {" "}
+                    Editar
+                  </Link>{" "}
+                  /{" "}
+                  <Link to={`/excluir/produtos/${item.id}`}>
+                    {" "}
+                    Excluir
+                  </Link>
+                </td>
+              </tr>
             ))}
-        </main>
-    )
-    
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td colSpan={6}>PRODUTOS</td>
+            </tr>
+          </tfoot>
+
+        </table>
+      </div>
+    </>
+  );
 }
