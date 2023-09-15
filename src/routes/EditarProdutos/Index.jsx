265c96013cac9e7b93b362ae16b02b7af24b92f7
Guilherme Dias Gomes <guilhermediasgomes2@gmail.com>
2023-09-15 15:07:20 -0300

    alterações na funcção de editar produtos

--- src/routes/EditarProdutos/Index.jsx
+++ src/routes/EditarProdutos/Index.jsx
@@ -1,27 +1,86 @@
-import { useNavigate, useParams } from "react-router-dom"
-import { listaProdutos } from "../../components/listaProdutos"
+import { useNavigate, useParams } from "react-router-dom";
+import { listaProdutos } from "../../components/listaProdutos";
+import { useState } from "react";
 
 export default function EditarProdutos() {
+  document.title = "Editar Produtos";
 
-    const lista = listaProdutos
+  //utilizando redirecionamento de ROTAS com useNavigate();
+  const navigate = useNavigate()
 
-    const navegacao = useNavigate()
-    const {id} = useParams()
+  //Utilizando o HOOK useParams()
+  const { id } = useParams();
 
-    const proc = lista.filter(prod => prod.id == id)
-    const produto = proc[0]
+  //Filtrando o produto selecionado byId
+  const produtoFiltrado = listaProdutos.filter(
+    (item) => item.id === parseInt(id)
+  )[0];
 
-    const salvar = () => {
-        alert(`Produto: ${produto.nome} editado com sucesso!`)
-        return navegacao('/produtos')
-    }
+  //Utilizando o HOOK useState()
 
-    return(
-        <main>
-            <h1>EditarProdutos</h1>
-            <p>Editando os dados do produto: {produto.nome}</p>
-            <button onClick={salvar}>Salvar</button>
-        </main>
-    )
-    
+  const [produto, setProduto] = useState({
+    id: produtoFiltrado.id,
+    nome: produtoFiltrado.nome,
+    img: produtoFiltrado.img,
+    preco: produtoFiltrado.preco,
+  });
+
+  const handleChange = (event)=>{
+      const {name,value} = event.target;
+      setProduto({...produto, [name]:value});
+  }
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+
+
+        let indice;
+
+        listaProdutos.forEach((item,index)=>{
+          if(item.id == produto.id){
+            indice = index;
+          }
+        });
+      listaProdutos.splice(indice,1,produto);
+
+      //Redirecionando após realizar a edição do produto filtrado.
+      navigate("/produtos");
+  }
+
+
+  return (
+    <>
+      <h1>Editar Produtos</h1>
+
+      <div>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Produto Selecionado</legend>
+            <div>
+              <label htmlFor="idNome">Nome</label>
+              <input type="text" name="nome" id="idNome" onChange={handleChange} value={produto.nome} />
+            </div>
+            <div>
+              <label htmlFor="idImg">Imagem</label>
+              <input type="url" name="img" id="idImg" onChange={handleChange} value={produto.img} />
+            </div>
+            <div>
+              <label htmlFor="idPreco">Preço</label>
+              <input
+                type="text"
+                name="preco"
+                id="idPreco" 
+                onChange={handleChange} 
+                value={produto.preco}
+              />
+            </div>
+            <div>
+              <button>EDITAR</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+ 
+    </>
+  );
 }
